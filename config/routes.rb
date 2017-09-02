7bd9cf15ebaf07410d5688777568dbab45d96e48
ken992 <fire09682@gmain.com>
2017-09-02 16:41:09 +0900

    5.3.2演習まで完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-#  binding.pry
-
   root 'static_pages#home'
-
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/addmember'
-  get 'static_pages/contact'
-  get 'static_pages/delmember'
-
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 end
