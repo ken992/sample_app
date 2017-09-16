0ae20e51ec167e4da6a4d340d53963025e391c7d
ken992 <fire09682@gmain.com>
2017-09-17 07:33:12 +0900

    add ja.yml

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,9 @@ Bundler.require(*Rails.groups)
 
 module SampleApp
   class Application < Rails::Application
-      config.active_record.default_timezone = :local
-  config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
