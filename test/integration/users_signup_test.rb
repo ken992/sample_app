d550e793888cb04ae7fc9b9f36837b6bfe0301cd
ken992 <fire09682@gmain.com>
2017-09-18 11:52:19 +0900

    7.3.4の演習1

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -6,10 +6,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name:  "",
-                                         email: "user@invalid",
-                                         password:              "foo",
-                                         password_confirmation: "bar" } }
+                                         email: "",
+                                         password:              "a111",
+                                         password_confirmation: "111" } }
     end
     assert_template 'users/new'
+    assert_template 'shared/_error_messages'
+    assert_template 'layouts/_rails_default'
+
+     assert_select "div" , /The form contains/
+     assert_select "div" , /The form contains [1-6] error/
+     assert_select "div#error_explanation li" , 1..6 
+#    assert_select 'div.<CSS class for field with error>'
+
   end
 end
