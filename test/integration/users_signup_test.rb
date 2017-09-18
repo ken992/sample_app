80a2862697110b3b7d0c695f0ba217d6eb75bc35
ken992 <fire09682@gmain.com>
2017-09-18 13:07:37 +0900

    7.3完了

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,7 +5,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:  "",
+      post signup_path, params: { user: { name:  "",
                                          email: "",
                                          password:              "a111",
                                          password_confirmation: "111" } }
@@ -14,10 +14,12 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'shared/_error_messages'
     assert_template 'layouts/_rails_default'
 
-     assert_select "div" , /The form contains/
-     assert_select "div" , /The form contains [1-6] error/
-     assert_select "div#error_explanation li" , 1..6 
-#    assert_select 'div.<CSS class for field with error>'
+    assert_select "div" , /The form contains/
+    assert_select "div" , /The form contains [1-6] error/
+    assert_select "div#error_explanation li" , 1..6 
+    assert_select 'form[action="/signup"]'
+
+#   assert_select 'div.<CSS class for field with error>'
 
   end
 end
