ada91ceab8004eb24ffdcebf79b8b067471533d4
ken992 <fire09682@gmail.com>
2017-10-29 14:47:20 +0900

    10.1.1 バグ修正 完了

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UsersEditTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "unsuccessful edit" do
+  #  get edit_user_path(@user)
+  #  assert_template 'users/edit'
+
+
+  end
+
 end
