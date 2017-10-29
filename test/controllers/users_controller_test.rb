be4e6fe7111e1b683a77115bc5622897afd4769f
ken992 <fire09682@gmail.com>
2017-10-29 21:47:30 +0900

    リスト10.22

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,26 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test 'should get new' do
     get signup_path
     assert_response :success
   end
 
+  test 'should redirect edit when not logged in' do
+    get edit_user_path(@user) # loginしないでedit画面へ遷移
+    assert_not flash.empty? # flashがemptyではない = errorが格納されている
+    assert_redirected_to login_url # login画面に転送される
+  end
+
+  test 'should redirect update when not logged in' do
+    patch user_path(@user), params: { user: { name: @user.name,       # loginしないでusers#update
+                                              email: @user.email } }
+    assert_not flash.empty? # flashがemptyではない = errorが格納されている
+    assert_redirected_to login_url # login画面に転送される
+  end
 end
