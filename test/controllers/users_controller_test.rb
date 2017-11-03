403e8de0d76c83f1cdfd22f5dc829e67d86678bd
ken992 <fire09682@gmail.com>
2017-11-03 21:54:31 +0900

    リスト10.40

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -8,6 +8,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should redirect index when not logged in' do
+    # 未loginでusers indexにaccsessするとlogin画面にredirect
     get users_path
     assert_redirected_to login_url
   end
