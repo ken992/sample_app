1f68491f36e24998c332cc799fdd675bf7c1119e
ken992 <fire09682@gmail.com>
2017-11-12 12:44:51 +0900

    リスト10.56

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -38,13 +38,22 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url # root画面へredirect login中なのでlogin画面へはredirectしない
   end
 
-  test "should redirect update when logged in as wrong user" do
+  test 'should redirect update when logged in as wrong user' do
     log_in_as(@other_user) # user2でlogin
     # loginしていない別のuserのurlでusers#update
     patch user_path(@user), params: { user: { name: @user.name,
                                               email: @user.email } }
     assert flash.empty?  # errorは格納されない
     assert_redirected_to root_url # root画面へredirect login中なのでlogin画面へはredirectしない
+  end
+
+  test 'should not allow the admin attribute to be edited via the web' do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: { user: { password: @other_user.password,
+                                                    password_confirmation: @other_user.password_confirmation,
+                                                    admin: true } }
+    assert_not @other_user.reload.admin?
   end
 
 end
