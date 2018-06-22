571456c16d6e70043c812e43cc34021abaabb0b2
ken992 <fire09682@gmail.com>
2018-06-22 22:44:10 +0900

    13.4.1完了

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -10,7 +10,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
-    assert_select 'input[type=FILL_IN]'
+    assert_select 'input[type="file"]'
     # 無効な送信
     post microposts_path, micropost: { content: "" }
     assert_select 'div#error_explanation'
@@ -18,20 +18,20 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     content = "This micropost really ties the room together"
     picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, micropost: { content: content, picture: FILL_IN }
+      post microposts_path, micropost: { content: content, picture: picture }
     end
-    assert FILL_IN.picture?
+    assert assigns(:micropost).picture?
     follow_redirect!
     assert_match content, response.body
     # 投稿を削除する
-    assert_select 'a', 'delete'
+    assert_select 'a', text: 'delete'
     first_micropost = @user.microposts.paginate(page: 1).first
     assert_difference 'Micropost.count', -1 do
       delete micropost_path(first_micropost)
     end
-    # 違うユーザーのプロフィールにアクセスする
+    # 違うユーザーのプロフィールにアクセス (削除リンクがないことを確認)
     get user_path(users(:archer))
-    assert_select 'a', { text: 'delete', count: 0 }
+    assert_select 'a', text: 'delete', count: 0
   end
 
   test "micropost sidebar count" do
