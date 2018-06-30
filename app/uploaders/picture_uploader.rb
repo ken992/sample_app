b92f87af6b3fc876c533f223ca283d54635852bc
ken992 <fire09682@gmail.com>
2018-06-30 18:36:15 +0900

    Support for uploading video

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,6 +1,8 @@
 class PictureUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::MiniMagick
+  include ApplicationHelper
+
   process resize_to_limit: [400, 400]
 
   # Include RMagick or MiniMagick support:
@@ -40,8 +42,12 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
-    %w(jpg jpeg gif png)
+    %w(jpg jpeg gif png mp4)
   end
+
+  # def content_type_whitelist
+  #   [/(image|video)/]
+  # end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
